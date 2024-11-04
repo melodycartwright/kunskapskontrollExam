d6a19c2bb31f1a02b054066fc99392a2c25fd7f0
Melody Cartwright <melody94c@gmail.com>
2024-11-04 23:10:14 +0100

    Building javascript function

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-const name = document.getElementById('name')
-const phone = document.getElementById('phone')
+const nameInput = document.getElementById('name')
+const phoneInput = document.getElementById('phone')
 const submit = document.getElementById('submit')
 const listName = document.getElementById('list-name')
 const listPhone= document.getElementById('list-phone')
+const contactInfo=document.getElementById('contact-form')
+const contactList = document.getElementById ('contact-list')
+
+submit.addEventListener(click, () => {
+
+    const name= nameInput.value.trim();
+    const phone = phoneInput.value.trim();
+
+    if (name && phone) {
+        contactInfo.innerHTML =
+         <div id="list-name"> ${name}</div>
+        <div id="list-phone">${phone} </div>
+        <div id="buttons">
+    <button id="change-btn">Ändra</button>
+            <button id="delete-btn">Radera</button>
+            </div>;
+contactList.appendChild(contactInfo);
+nameInput.value= '';
+phoneInput.value='';
+
+    }
+})

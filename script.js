1ad3c2aa0fda12f5cb3db658891e1f00f41774d1
Melody Cartwright <melody94c@gmail.com>
2024-11-07 17:39:47 +0100

    completing functionality of js

--- script.js
+++ script.js
@@ -28,13 +28,20 @@ const deleteBtn= document.querySelector('delete-btn')
         deleteBtn.addEventListener('click', () => contactInfo.remove());
     }
 })*/
-const nameInput = document.getElementById('name')
-const phoneInput = document.getElementById('phone')
-const submit = document.getElementById('submit')
+const nameInput = document.getElementById('name');
+const phoneInput = document.getElementById('phone');
+const submit = document.getElementById('submit');
+const mainSection=document.getElementById('main-section');
 
 submit.addEventListener('click', function(renderList) {
     renderList.preventDefault();
-    alert('clicked submit')
+     const name = nameInput.value;
+    const phone = phoneInput.value;
+    
+     if (!name || !phone) {
+        alert('Please enter both name and phone number');
+        return;
+    }
 
     const contactList = document.createElement ('div');
     contactList.className='contact-list';
@@ -42,24 +49,80 @@ submit.addEventListener('click', function(renderList) {
     const contactInfo = document.createElement('ul');
     contactInfo.className='contact-info';
 
+
     const listedName = document.createElement ('li');
     listedName.className= 'listed-name';
+    listedName.innerText= name;
+   
 
     const listedNumber= document.createElement('li');
     listedNumber.className='listed-number';
+    listedNumber.innerText=phone;
+
+
+
     const changeBtn= document.createElement('button');
-    changeBtn.classname='change-btn';
+    changeBtn.className='change-btn';
     changeBtn.innerHTML= '<p> Ändra </p>';
+   
     const deleteBtn= document.createElement('button');
-    deleteBtn.classname='delete-btn';
+    deleteBtn.className='delete-btn';
     deleteBtn.innerHTML= '<p> Radera </p>';
-    contactList.appendChild(contactInfo);
+
+   
     contactInfo.appendChild(listedName);
-    contactInfo.appendChild(listedNumber);
+     contactInfo.appendChild(listedNumber);
     contactInfo.appendChild(changeBtn);
     contactInfo.appendChild(deleteBtn);
-    document.getElementById('main-section').appendChild(contactList);
+     contactList.appendChild(contactInfo);
+     mainSection.appendChild(contactList);
+   
+nameInput.value= '';
+phoneInput.value= '';
+
+changeBtn.addEventListener('click',function() {
+  changeBtn.innerHTML= '<p>Spara</p>';
+  const cancelBtn = document.createElement('button');
+  cancelBtn.className='cancel-btn';
+  cancelBtn.innerHTML='<p>Avbryt</p>';
+
+  const nameChangeInput= document.createElement('input');
+  nameChangeInput.type = 'text';
+  nameChangeInput.value= listedName.innerText;
+
+ const phoneChangeInput= document.createElement('input');
+  phoneChangeInput.type = 'text';
+  phoneChangeInput.value= listedNumber.innerText;
+
+  contactInfo.replaceChild(nameChangeInput, listedName);
+  contactInfo.replaceChild (phoneChangeInput, listedNumber);
+contactInfo.appendChild(cancelBtn);
+
+changeBtn.addEventListener('click', function saveChanges(){
+
+    listedName.innerText= nameChangeInput.value;
+    listedNumber.innerText= phoneChangeInput.value;
+
+    contactInfo.replaceChild(listedName, nameChangeInput);
+    contactInfo.replaceChild (listedNumber, phoneChangeInput);
+
+    changeBtn.innerHTML='<p>Ändra</p>';
+    cancelBtn.remove();
+
+    changeBtn.removeEventListener('click', saveChanges);
+});
+  cancelBtn.addEventListener('click', function () {
+        
+            contactInfo.replaceChild(listedName, nameChangeInput);
+            contactInfo.replaceChild(listedNumber, phoneChangeInput);
+
+          
+            changeBtn.innerHTML = '<p>Ändra</p>';
+            cancelBtn.remove();
+        }); });
+deleteBtn.addEventListener('click', function(){
+mainSection.removeChild(contactList);
+});});
 
 
-    console.log(contactList);
-})
+ 
